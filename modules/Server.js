9bddcc512c0622fd9aea91aceda18211487ab77e
Nicolas fernandes <nicolas.fernandes.etu@univ-lille.fr>
2021-02-09 19:24:12 +0100

    worker thread

--- modules/Server.js
+++ modules/Server.js
@@ -1,21 +1,33 @@
 import { createServer } from 'net';
+import { Worker } from 'worker_threads';
+
 
 export default class Server {
 
     constructor() {
-        this.server = createServer(socket => this.init(socket));
+        this.server = createServer(async socket => await this.clientConnected(socket).catch(err => console.error({err})));
         this.server.listen(process.env.PORT, () => console.log('Server created'));
+    }
+
+    clientConnected(socket) {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker('./modules/clientConnected.js', { workerData });
+            worker.on('message', resolve);
+            worker.on('error', reject);
+            worker.on('exit', (code) => {
+                if (code !== 0)
+                    reject(new Error(`Worker stopped with exit code ${code}`));
+            })
+        })
     }
 
     init(socket) {
         this.socket = socket;
         console.log('Client connected');
         socket.on('data', (data) => {
-            console.log('Data sent to server : ' + data);
-
-            //echo data
-            const is_kernel_buffer_full = socket.write('Data ::' + data);
-            if(is_kernel_buffer_full){
+            
+            const is_kernel_buffer_full = this.serverResponse(data, socket);
+            if (is_kernel_buffer_full) {                
                 console.log('Data was flushed successfully from kernel buffer i.e written successfully!');
             }else{
                 socket.pause();
@@ -23,6 +35,27 @@ export default class Server {
         })
         socket.on('end', () => console.log('Closed'));
         this.handShake(socket);
+    }
+
+    serverResponse(data, socket) {
+        let response;
+        console.log(data.toString());
+        switch (data.toString().split(" ")[0]) {
+            case "AUTH":
+                response = "530 Please login with USER and PASS.\n";
+                break;
+            case "USER":
+                
+            case "PASS" :
+            case "PWD" :
+            case "CWD" :
+            case "CDUP":
+            case "LIST":
+            default:
+                response = "530 Oh shit ."
+                
+        }
+        return socket.write(response);
     }
 
     async handShake(socket) {
