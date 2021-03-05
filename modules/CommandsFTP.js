5c7d0850d9e95cb73ecddf414ea222b5c6f0b098
matryxxx02 <nicolas.fernandes.etu@univ-lille.fr>
2021-03-05 18:42:43 +0100

    implement fs

--- modules/CommandsFTP.js
+++ modules/CommandsFTP.js
@@ -1,10 +1,12 @@
 import registry from "./commandsRegistry"
 import { write } from '../utils/responseUtils.js'
+import FileSystem from "./FileSystem";
 
 export default class CommandsFTP {
     constructor(connection) {
         this.commandHistory = {}
         this.connection = connection;
+        this.fs = new FileSystem()
     }
 
     parseClientResponse = (data) => {
@@ -19,7 +21,12 @@ export default class CommandsFTP {
         const clientRequest = this.parseClientResponse(req);
         const command = registry[clientRequest.command];
 
-        if (command) command.handler({ socket: this.connection.commandSocket, connection : this.connection, message: clientRequest.message }, write);
+        if (command) command.handler({
+            socket: this.connection.commandSocket,
+            connection: this.connection,
+            message: clientRequest.message,
+            fs: this.fs
+        }, write);
         else write(this.connection.commandSocket, "530 notexist")
     }
 }
